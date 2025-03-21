b93f44929428ddd1ef0680568ad5cd9642a41292
Guilherme Vianna <59426923+Guilherme-Vianna@users.noreply.github.com>
2025-03-21 12:05:53 -0300

    chore: test webhook

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -6,6 +6,7 @@ import { Prisma } from '@prisma/client';
 export class OrderController {
   constructor(private readonly orderService: OrderService) { }
 
+  
   @Post()
   create(@Body() data: Prisma.orderCreateInput) {
     return this.orderService.create(data);
